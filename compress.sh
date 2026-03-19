3ebc36dc5d8d49487b707d7092d1dbba2177dd2d
sujeitoculto <silva.zanin@gmail.com>
2026-03-19 10:45:03 -0300

    Add compactor

---
+++ compress.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script para compactar standard-ms excluindo node_modules e .env
+
+SOURCE_DIR="standard-ms"
+OUTPUT_FILE="standard-ms.tar.gz"
+
+echo "Compactando $SOURCE_DIR..."
+echo "Arquivo de saída: $OUTPUT_FILE"
+
+tar -czf "../$OUTPUT_FILE" \
+  --exclude="$SOURCE_DIR/luciluci-docs" \
+  --exclude="$SOURCE_DIR/node_modules" \
+  --exclude="$SOURCE_DIR/**/node_modules" \
+  --exclude="$SOURCE_DIR/.env" \
+  --exclude="$SOURCE_DIR/**/.env" \
+  --exclude="$SOURCE_DIR/.git" \
+  "../$SOURCE_DIR"
+
+if [ $? -eq 0 ]; then
+  echo "✓ Compactação concluída com sucesso!"
+  echo "✓ Arquivo criado: $OUTPUT_FILE"
+  ls -lh "../$OUTPUT_FILE"
+else
+  echo "✗ Erro na compactação"
+  exit 1
+fi
